0ded15e33fed4c0546608c73aa0ce2f140e61d09
CleanOak <53487230+CleanOak@users.noreply.github.com>
2024-01-26 18:51:54 +0000

    Add signup functioin

--- run.py
+++ run.py
@@ -12,10 +12,29 @@ SCPOED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCPOED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Authenticate')
 
-user_info = SHEET.worksheet('user_info')
+#user_info = SHEET.worksheet('user_info')
 
-data = user_info.get_all_values()
+#data = user_info.get_all_values()
 
-print(data)
+print("LOGIN AUTHENTICATION AND ACCESS CONTROL MODULE\n")
+
+
+def signup():
+    user_name = input("Enter your user name: \n")
+    email = input("Enter email address: \n")
+    passwd = input("Enter password: \n")
+    conf_passwd = input("Confirm password: \n")
+
+    if conf_passwd == passwd:
+
+        print("You have registered successfully!")
+
+    else:
+        print("Password is not the same as above!")
+
+signup()
+        
+
+
 
 
