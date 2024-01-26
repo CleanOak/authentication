0f796bb18f10adbb984fe18ee0c4bf0e420c8480
CleanOak <53487230+CleanOak@users.noreply.github.com>
2024-01-26 15:51:13 +0000

    Set up API connection to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
-print ("This is my third project")
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCPOED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCPOED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Authenticate')
+
+user_info = SHEET.worksheet('user_info')
+
+data = user_info.get_all_values()
+
+print(data)
+
+
