95bb103bc8979da02182a643ce14a595181e6924
CleanOak <53487230+CleanOak@users.noreply.github.com>
2024-01-27 22:42:41 +0000

    Add user prompt function

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import re
+# from distutils.util import strtobool as stb 
+import setuptools
+import sys
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -19,18 +23,35 @@ data = user_info.get_all_values()
 
 print("LOGIN AUTHENTICATION AND ACCESS CONTROL MODULE\n")
 
+def userPrompt():
+     """
+     Get user response
+     """
+     
+     try:
+          
+            userAnswer  = input("Are you a new or existing user? y/n\n")
+            if userAnswer.lower() == 'y':
+                return 1
+            elif userAnswer.lower() == 'n':
+                return 0
+          
+     except ValueError:("Please enter y or n")
+          
+          
+   
 
-    
-    
 
 def login(login_data):
             
-            while True:
-            
+            """
+            Get user credentials to login
+            """   
+            while True: 
+
                 user_name = input("Enter your username: \n")
                 passwd = input("Enter password: \n")
                 try:
-    
                     for creds in login_data:
                         if (user_name in creds[0] and passwd in creds[2]):  
                             user1 = creds[0]
@@ -39,15 +60,19 @@ def login(login_data):
                     if (user1 == user_name and passwd1 == passwd):
                         print ("You have logged in sucessfully")
 
-                        break       
+                        break
+                                
                 except:
-                    
-                    print("Your details do not match please sign up")
+                    print("Your username or password does not match please try again\n")
+
+
+def update_spreadsheet(data, worksheet):
+     
+     print(f'Updating {worksheet} worksheet.\n')
+     update_to_spreadsheet =  SHEET.worksheet(worksheet)
+     update_to_spreadsheet.append_row(data)
+     print(f'{worksheet} worksheet has updated successfully')
                 
-            
-                                  
-login(data)
-#print(data)
 
 # def signup():
 #     while True:
@@ -62,7 +87,17 @@ login(data)
 #             else:
 #                 print("Password is not the same as above!")
 
-# signup()
+answer = userPrompt()
+
+if answer == 1:
+     login(data)
+
+elif answer == 0:
+    print("please sign up")
+
+
+
+
         
 
 
