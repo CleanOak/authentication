03a5a3ae69ea4f8ec34f5c3092e5fed81d278201
CleanOak <53487230+CleanOak@users.noreply.github.com>
2024-01-27 14:23:02 +0000

    Add login function to file

--- run.py
+++ run.py
@@ -13,34 +13,53 @@ SCPOED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCPOED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Authenticate')
 
-#user_info = SHEET.worksheet('user_info')
+user_info = SHEET.worksheet('user_info')
 
-#data = user_info.get_all_values()
+data = user_info.get_all_values()
 
 print("LOGIN AUTHENTICATION AND ACCESS CONTROL MODULE\n")
 
 
-def signup():
-    while True:
+    
+    
 
-            user_name = input("Enter your username: \n")
-            email = input("Enter email address: \n")
-            pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
-            if re.match(pattern, email):
-                 print("This is correct")
+def login(data):
+    user_name = input("Enter your username: \n")
+    passwd = input("Enter password: \n")
+
+    for x in data:
+        if (user_name == x[0] and passwd == x[2]):
+            print("Login succesful")
+                   
+                
+        else:
+         print("Username or password incorrect")
         
-                 
-            passwd = input("Enter password: \n")
-            conf_passwd = input("Confirm password: \n")
+    
 
-            if conf_passwd == passwd:
 
-                print("You have registered successfully!")
+login(data)
 
-            else:
-                print("Password is not the same as above!")
 
-signup()
+
+
+
+
+
+# def signup():
+#     while True:
+
+#             user_name = input("Enter your username: \n")
+#             email = input("Enter email address: \n")             
+#             passwd = input("Enter password: \n")
+#             conf_passwd = input("Confirm password: \n")
+
+#             if conf_passwd == passwd:
+#                 print("You have registered successfully!")
+#             else:
+#                 print("Password is not the same as above!")
+
+# signup()
         
 
 
