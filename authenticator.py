40673c685a1ac330cc1e7eb945690495271f8249
CleanOak <53487230+CleanOak@users.noreply.github.com>
2024-02-06 22:19:17 +0000

    Add comments and clear white trailing spaces

--- authenticator.py
+++ authenticator.py
@@ -45,7 +45,6 @@ def login(login_user_data):
     x=0
     while x==0:
         print("Please enter your details below to login to the game....\n")
-            
         user_name = input("Enter your username: \n")
         passwd = input("Enter password: \n")
         try:
@@ -69,7 +68,6 @@ def signup():
 
         print("Please enter an email with the format name@some_address.com")
         email_address = input("Enter your email address: \n")
-        
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
         
         if re.fullmatch(regex, email_address):
@@ -83,10 +81,8 @@ def signup():
         print("Your password must be 8 characters long, must contain a capital letter(s),")
         print("small letter(s) and at least a number and special characters")
         print("For example:XdsE83&! \n")
-        
-       
         new_passwd = input("Enter password:\n")
-            
+        
         #compiling regex
         reg_pass = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?& ])[A-Za-z\d@$!#%*?&]{8,18}$"
         match_re = re.compile(reg_pass)
@@ -96,15 +92,14 @@ def signup():
 
         #validating conditions
         if reg_search:
-            print("Password is valid") 
+            print("Password is valid")
             break
-        else:    
+        else:
             print("Password is invalid")
 
     conf_passwd = input("Confirm password: \n")
     if conf_passwd == new_passwd:
-        print("Password matched!")
-                    
+        print("Password matched!\n")
         print(f'Your username {new_username} and password has been stored successfully!!!\n')
 
     else:
@@ -112,5 +107,3 @@ def signup():
 
     data = [new_username,email_address,conf_passwd]
     update_spreadsheet(data)
-
-
