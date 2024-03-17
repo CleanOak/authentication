6dadffb37f614564bfe8bc685b7a786ffca37d24
Morgan Asare <ohenemorgan@gmail.com>
2024-03-17 19:52:16 +0000

    Troubleshooting signup function

--- authenticator.py
+++ authenticator.py
@@ -72,14 +72,14 @@ def signup(user_data):
             if new_username == "":
                 print("Please enter a valid value for the username\n")
                 continue
-                if new_username == data[0]:
-                    print("User exists please use a differnt username\n")
-                    time.sleep(0.5)
-                    signup(login_data())
-                    break    
-                print("Please enter an email with the format name@some_address.com")
-                email_address = input("Enter your email address: \n")
-                regex = r'\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,7}\b'
+        #         if new_username == data[0]:
+        #             print("User exists please use a differnt username\n")
+        #             time.sleep(0.5)
+        #             signup(login_data())
+        #             break    
+            print("Please enter an email with the format name@some_address.com")
+            email_address = input("Enter your email address: \n")
+            regex = r'\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,7}\b'
         
         if re.fullmatch(regex, email_address):
                 
