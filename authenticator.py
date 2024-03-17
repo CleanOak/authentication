cc2f082b0e06faf0efd42caaf3940d4ab061fcc8
Morgan Asare <ohenemorgan@gmail.com>
2024-03-17 20:01:00 +0000

    Troubleshoot signup function

--- authenticator.py
+++ authenticator.py
@@ -81,9 +81,9 @@ def signup(user_data):
         #             time.sleep(0.5)
         #             signup(login_data())
         #             break    
-        print("Please enter an email with the format name@some_address.com")
-        email_address = input("Enter your email address: \n")
-        regex = r'\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,7}\b'
+            print("Please enter an email with the format name@some_address.com")
+            email_address = input("Enter your email address: \n")
+            regex = r'\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,7}\b'
         
         if re.fullmatch(regex, email_address):
                 
