ac77f858efddc025cb2c61a93c02bfb9ac14eb72
Morgan Asare <ohenemorgan@gmail.com>
2024-03-18 12:10:52 +0000

    Troubleshoot signup email while loop

--- authenticator.py
+++ authenticator.py
@@ -87,14 +87,14 @@ def signup(user_data):
         
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'   
 
-        while True:
-            
-            if re.fullmatch(regex, email_address):  
-                print("Email format accepted\n")
-                break
-            else:
-                print("Please enter a valid email format with the format name@some_address.com")
-                break
+    while True:
+        
+        if re.fullmatch(regex, email_address):  
+            print("Email format accepted\n")
+            break
+        else:
+            print("Please enter a valid email format with the format name@some_address.com")
+            break
 
     while True:
         print("Your password must be 8 characters long, must contain a capital letter(s),")
