00e56a6252947918097384e029cb0e3fa084bfaf
Morgan Asare <ohenemorgan@gmail.com>
2024-03-19 17:13:14 +0000

    Add advanced game function to game.py

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from authenticator import signup
 from authenticator import login_data
 from game import level_banner
 from game import choose_level
+from game import play_guess_advanced
 
 # A list of countries to be chosen at random
 countries = ['England', 'Ghana', 'America', 'Nigeria',
@@ -79,65 +80,65 @@ def new_user():
     login(login_data())
     game_banner()
 
-def select_random_word():
-    """
-    Select random word from list words and return in upper case
-    """
-    random_word = random.choice(countries)
-    return random_word.upper()
+# def select_random_word():
+#     """
+#     Select random word from list words and return in upper case
+#     """
+#     random_word = random.choice(countries)
+#     return random_word.upper()
 
 
-def play_guess_country(random_word):
-    """
-    Function for the game which inherit randomWord from randomWord function
-    and run a while loop until no tries left. The function displays the game
-    as the loop runs and validates if inputs by user is wrong and prompt
-    users of the errors
-    """
-    guessed_letters = countries
+# def play_guess_country(random_word):
+#     """
+#     Function for the game which inherit randomWord from randomWord function
+#     and run a while loop until no tries left. The function displays the game
+#     as the loop runs and validates if inputs by user is wrong and prompt
+#     users of the errors
+#     """
+#     guessed_letters = countries
 
-    tries = 10
-    print(f"Total Attempts: {tries}\n")
-    print(" _ " * len(random_word))
+#     tries = 10
+#     print(f"Total Attempts: {tries}\n")
+#     print(" _ " * len(random_word))
 
-    while tries > 0:
-        wrong_letter_count = 0
-        guess = input(" Please enter your guess: ").upper()
+#     while tries > 0:
+#         wrong_letter_count = 0
+#         guess = input(" Please enter your guess: ").upper()
 
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessed_letters:
-                print(f"{guess} is already guessed!")
-                print(f"\n Attempt left: {tries}\n")
-            elif guess not in random_word:
-                print("\n You guessed wrong. Try Again")
-                tries -= 1
-                guessed_letters += guess
-                print(f"\n Attempt left: {tries}\n")
-            else:
-                print(f"\n Correct! {guess} is in the word.")
-                print(f"\n Attempt left: {tries}\n")
-        else:
-            print("\n Invalid input. Enter only one alphabet")
-            print(f"\n Attempt left: {tries}\n")
-        guessed_letters += guess
+#         if len(guess) == 1 and guess.isalpha():
+#             if guess in guessed_letters:
+#                 print(f"{guess} is already guessed!")
+#                 print(f"\n Attempt left: {tries}\n")
+#             elif guess not in random_word:
+#                 print("\n You guessed wrong. Try Again")
+#                 tries -= 1
+#                 guessed_letters += guess
+#                 print(f"\n Attempt left: {tries}\n")
+#             else:
+#                 print(f"\n Correct! {guess} is in the word.")
+#                 print(f"\n Attempt left: {tries}\n")
+#         else:
+#             print("\n Invalid input. Enter only one alphabet")
+#             print(f"\n Attempt left: {tries}\n")
+#         guessed_letters += guess
 
-        for letter in random_word:
-            if letter in guessed_letters:
-                print(f"{letter}", end=' ')
-            else:
-                print(" _ ", end=' ')
-                wrong_letter_count += 1
+#         for letter in random_word:
+#             if letter in guessed_letters:
+#                 print(f"{letter}", end=' ')
+#             else:
+#                 print(" _ ", end=' ')
+#                 wrong_letter_count += 1
 
-        if wrong_letter_count == 0:
-            print(f"\nCongrats. The secret word is {random_word}.")
-            print("\nHURAAYYY!!! You Guessed correctly :)")
-            break
-        if tries == 0:
-            print("\n")
-            print("OOO! You lost this time!!! Better luck next time :)\n")
-            print (f"The correct word is {random_word}.\n")
+#         if wrong_letter_count == 0:
+#             print(f"\nCongrats. The secret word is {random_word}.")
+#             print("\nHURAAYYY!!! You Guessed correctly :)")
+#             break
+#         if tries == 0:
+#             print("\n")
+#             print("OOO! You lost this time!!! Better luck next time :)\n")
+#             print (f"The correct word is {random_word}.\n")
 
-    return tries
+#     return tries
 
 
 def restart_game():
@@ -155,7 +156,7 @@ def restart_game():
             print("Game Started")
             time.sleep(1)
             game_banner()
-            play_guess_country(select_random_word())
+            choose_level()
         elif answer.upper() == "N":
             time.sleep(1.5)
             print("\nGood Bye!!")
@@ -176,7 +177,7 @@ def main():
     app_banner()
     login_check()
     choose_level()
-    play_guess_country(select_random_word())
+    # play_guess_country(select_random_word())
     restart_game()
 
 main()
