9a00901dae715a8fb6ce7d420fe4cc134967f3ac
Morgan Asare <ohenemorgan@gmail.com>
2024-03-19 12:56:10 +0000

    Add level banner function to inform users what to do

---
+++ game.py
@@ -0,0 +1,6 @@
+def level_banner():
+    print("Choose a level from below...\n")
+    print("Select Easy or Advanced \n")
+    print("Please type in E to play 'Easy Level' or A to play 'Advanced Leve'\n")
+
+def choose_Level():
